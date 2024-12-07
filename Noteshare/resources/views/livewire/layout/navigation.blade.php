52800b3e47f306d1603c64f9ffd2e79941fa5ef9
Shayan Najiseyedhosseinzadeh <shayan.najiseyedhosseinzadeh@student.howest.be>
2024-12-07 17:37:50 +0100

    Load notes in index view

--- Noteshare/resources/views/livewire/layout/navigation.blade.php
+++ Noteshare/resources/views/livewire/layout/navigation.blade.php
@@ -41,7 +41,7 @@ new class extends Component
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
+                <x-standard-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
@@ -66,7 +66,7 @@ new class extends Component
                             </x-dropdown-link>
                         </button>
                     </x-slot>
-                </x-dropdown>
+                </x-standard-dropdown>
             </div>
 
             <!-- Hamburger -->
