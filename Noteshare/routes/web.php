2775ef2d646846f3fa781f422759dd150ed23110
Shayan Najiseyedhosseinzadeh <shayan.najiseyedhosseinzadeh@student.howest.be>
2024-12-07 17:10:39 +0100

    Update dashboard and install wireui

--- Noteshare/routes/web.php
+++ Noteshare/routes/web.php
@@ -8,6 +8,12 @@ Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
 
+Route::group(['auth', 'verified'], function () {
+
+Route::view('notes', "notes.index")->name('notes.index');
+
+});
+
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
